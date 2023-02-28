b641dcee6ff3391c53249cda7472f215596fa23d
Roman <chv-roman@mail.ru>
2023-02-28 16:02:53 +0300

    fix: add\edit-point price

--- src/view/add-edit-point-view.js
+++ src/view/add-edit-point-view.js
@@ -89,7 +89,7 @@ const createAddEditPointTemplate = (offers, point) =>{
           <span class="visually-hidden">Price</span>
           &euro;
         </label>
-        <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
+        <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${basePrice}">
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit" ${isDisabledSubmit ? 'disabled' : ''}>Save</button>
